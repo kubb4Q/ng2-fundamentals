b4dbadde47dc64a3c73fb2a4284ef25a24da2723
KubbaQ <jakonn@gmail.com>
2017-08-10 20:42:49 +0200

    added imporvements in validation in profile page(more work on model driven forms)

--- app/user/user.module.ts
+++ app/user/user.module.ts
@@ -1,15 +1,17 @@
-import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './login.component';
-import { ProfileComponent } from './profile.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'
+
+import { LoginComponent } from './login.component';
+import { ProfileComponent } from './profile.component';
 import { userRoutes } from './user.routes'
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
