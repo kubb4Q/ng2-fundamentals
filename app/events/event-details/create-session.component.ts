b7d0f17530a8560dfd99cd47313b89b0d76965bd
KubbaQ <jakonn@gmail.com>
2017-08-21 21:19:07 +0200

    added better validation and custom validation to form

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -4,6 +4,28 @@ import { ISession } from '../shared/index'
 
 @Component({
   templateUrl: '/app/events/event-details/create-session.component.html',
+  styles: [`
+  em {
+      float: right;
+      color: #e05c65;
+      padding-left: 10px;
+    }
+  .error input, .error select, .error textarea{
+    background-color: #e3c3c5
+  }
+  .error ::-webkit-input-placeholder {
+    color: #999;
+  }
+  .error ::-moz-placeholder{
+    color: #999;
+  }
+  .error :-moz-placeholder {
+    color: #999;
+  }
+  .error :ms-input-placeholder {
+    color: #999;
+  }
+`]
 })
 export class CreateSessionComponent implements OnInit {
   newSessionForm: FormGroup
@@ -18,7 +40,7 @@ export class CreateSessionComponent implements OnInit {
     this.presenter = new FormControl('', Validators.required)
     this.duration = new FormControl('', Validators.required)
     this.level = new FormControl('', Validators.required)
-    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)])
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords(['foo', 'bar'])])
 
     this.newSessionForm = new FormGroup({
       name: this.name,
@@ -27,6 +49,18 @@ export class CreateSessionComponent implements OnInit {
       level: this.level,
       abstract: this.abstract
     })
+  }
+
+  private restrictedWords(words) {
+    return (control: FormControl): { [key: string]: any } => {
+      if (!words) return null
+
+      let invalidWords = words.map(w => control.value.includes(w) ? w : null).filter(w => w != null)
+
+      return invalidWords && invalidWords.length > 0
+        ? { 'restrictedWords': invalidWords.join(', ') }
+        : null
+    }
   }
 
   saveSesion(formValues) {
@@ -39,5 +73,7 @@ export class CreateSessionComponent implements OnInit {
       abstract: formValues.abstract,
       voters: []
     }
+
+    console.log(session)
   }
 }
